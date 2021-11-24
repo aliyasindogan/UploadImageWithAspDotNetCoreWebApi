3af47690cddb75aa1feb2ae74e7a04cdc1e69473
aliyasindogan <a.yasindogan@gmail.com>
2021-11-24 22:19:53 +0300

    VisualStudioProvider metodu düzenlendi.

---
+++ WinForm/VisualStudioProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace WinForm
+{
+    public class VisualStudioProvider
+    {
+        public static DirectoryInfo TryGetSolutionDirectoryInfo(string currentPath = null)
+        {
+            var directory = new DirectoryInfo(
+                currentPath ?? Directory.GetCurrentDirectory());
+            while (directory != null && !directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+            return directory;
+        }
+    }
+}
